f48799c1345ac6e1273a187b6ec3eb77efdb9098
vhorvath-gs <vhorvath@graphisoft.com>
2022-12-16 13:40:51 +0100

    Specify Big Sur (11.0) as the deployment target
    The MacOS release binaries are built on GitHub Actions' macos-11 image
    and the intended minimum version is 11 due to the ARM64 support.

--- .github/scripts/cache.ps1
+++ .github/scripts/cache.ps1
@@ -15,7 +15,7 @@ if ($env:RUNNER_OS -ceq 'Windows') {
   $h['CMAKE_EXE_LINKER_FLAGS_RELEASE'] = '-s'
 } elseif ($env:RUNNER_OS -ceq 'macOS') {
   $h['CMAKE_OSX_ARCHITECTURES'] = $Args[0]
-  # $h['CMAKE_XCODE_ATTRIBUTE_MACOSX_DEPLOYMENT_TARGET'] = '???'
+  $h['CMAKE_OSX_DEPLOYMENT_TARGET'] = '11.0'
   $h['CMAKE_CXX_FLAGS'] = '-fexceptions'
   $c = '-DNDEBUG -O3 -flto=thin'
   $h['CMAKE_C_FLAGS_RELEASE'] = $c
